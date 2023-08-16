20dd7da09d92dcde7fc5f1a7904c5bf999538b85
gabrielblasina <5722@holbertonstudents.com>
2023-08-16 13:10:48 -0700

    task-0 midifications

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
 
 from flask import Flask
 
@@ -7,8 +13,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
